2582b37260f397ae9065e8d286ebd88a19e4174e
gajjargaurav <gajjargaurav@gmail.com>
2015-01-30 17:38:16 +0000

    added mongodb dependency and connection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 var fs = require('fs');
 var _ = require('lodash');
-//var mongo = require('mongodb').MongoClient;
+var mongo = require('mongodb').MongoClient;
 
 var config_vars = {
 	"connectionUrl": "mongodb://127.0.0.1:27017/",
@@ -10,9 +10,14 @@ var config_vars = {
 
 function dummy(config){
 	var dummy = {};
-
+	config_vars = _.assign(config_vars, JSON.parse(config));
+	
 	function setup(cb){
-		cb(null,_.assign(config_vars, JSON.parse(config)));
+		mongo.connect(config_vars.connectionUrl, function(err, db){
+			if (err) { return console.error(err); }
+			cb(null,db);
+			db.close();	
+		});
 	}
 
 	function destroy(){
